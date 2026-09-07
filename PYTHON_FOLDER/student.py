491449aa8446acd60f56cdad66eec90fb9c44dcc
Harsh Chauhan <harshsan09876@gmail.com>
2026-09-08 00:17:11 +0530

    THIS FILE CONTAINS INFORMATION ABOUT STUDENTS ACADEMICS

---
+++ PYTHON_FOLDER/student.py
@@ -0,0 +1,12 @@
+students = [
+    {
+        "name": "Harsh Chauhan",
+        "branch": "CSE",
+        "percentage": 75
+    },
+    {
+        "name": "Jatin Chaudhary",
+        "branch": "CSE",
+        "percentage": 72
+    }
+]

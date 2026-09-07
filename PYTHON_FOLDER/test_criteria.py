f170682d8685b7329fce25fc2c5300bfcd552efc
Harsh Chauhan <harshsan09876@gmail.com>
2026-09-08 00:24:47 +0530

    COMPANIES CHECK THE CRITERIA FOR TESTING THE ELIGIBILITY OF CANDIDATE

---
+++ PYTHON_FOLDER/test_criteria.py
@@ -0,0 +1,18 @@
+# Store easier criteria for project testing
+test_criteria = {
+
+    # TCS test eligibility
+    "TCS": 55,
+
+    # Infosys test eligibility
+    "Infosys": 60,
+
+    # Google test eligibility
+    "Google": 7.5,
+
+    # Microsoft test eligibility
+    "Microsoft": 8.0,
+
+    # Learn to Earn Lab test eligibility
+    "Learn to Earn Lab": 7.0
+}
